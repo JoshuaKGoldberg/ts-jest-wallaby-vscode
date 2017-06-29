748e0c1e9b7ef568608f7ac18c56f5f846deaf54
Alena Gilevskaya <alena@gilevskaya.ru>
2017-06-29 16:11:31 +0300

    tsconfig fix

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import sum from "./sum"
 
-console.log('Hello, world!', sum(42, 7))
+// console.log('Hello, world!', sum(42, 7))
+
+let matt = { 
+  name: "Matt", 
+  friends: ["Mark", "Lyle", "Rian"], 
+  printFriends() { 
+    this.friends.forEach(f => 
+      console.log(this.name + " knows " + f)); 
+  } 
+} 
+matt.printFriends()
