b21b4b8c8c3e94c188974ab8f6f190ac0b7ddf05
Alena Gilevskaya <alena@gilevskaya.ru>
2017-07-11 20:34:59 -0700

    small fixes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
 import sum from "./sum"
 
-// console.log('Hello, world!', sum(42, 7))
-
-let matt = { 
-  name: "Matt", 
-  friends: ["Mark", "Lyle", "Rian"], 
-  printFriends() { 
-    this.friends.forEach(f => 
-      console.log(this.name + " knows " + f)); 
-  } 
-} 
-matt.printFriends()
+console.log('Hello, world!', sum(42, 7))
